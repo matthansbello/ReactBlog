4c27bff9ab0eec3a18cebb3d7c7996b1e08a1ea8
Matthew Hans-Bello <matthewhansbello@live.com>
2023-04-11 15:22:05 +0100

    d

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar  from './navbar';
+import Home from './Home';
 
 function App() {
+  const title = "Welcome to the new Blog";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      
+
+      <div className="content">
+        <Home />
+      </div>
+      
     </div>
   );
 }
 
 export default App;
+
