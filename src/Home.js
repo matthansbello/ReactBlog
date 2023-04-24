1ac058be706af5e2426b91d972f4b9cf4457193e
Matthew Hans-Bello <matthewhansbello@live.com>
2023-04-24 21:13:42 +0100

    Home Page

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
+
 const Home = () => {
+
+       // let name = 'mario';
+
+       const [name, setName] = useState('mario');
+       canst [AbstractRange, setAge] = useState(25);
+
+        const handleClick = ()=> {
+                setName("Luigi");
+                setAge(30);
+        }
+
+
         return ( 
                 <div className="home">
                         <h2>HomePage</h2>
+                        <p>{name} is {age} years old</p>
+                        <button onClick={handleClick}>Click Me</button> 
                 </div>
          );
-}
+} 
  
 export default Home;
